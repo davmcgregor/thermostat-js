bcfb3ac1b3782b69e4152c198be3b2f7eec7af2e
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-02-18 21:43:00 +0000

    Set minimum temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,7 +8,7 @@ describe('Thermostat',function(){
   });
 
   it ('starts at 20 degrees', function(){
-    expect(thermostat.temp).toEqual(20);
+    expect(thermostat.getCurrentTemp()).toEqual(20);
   });
   
   it ('can increase the temperature', function(){
@@ -19,6 +19,13 @@ describe('Thermostat',function(){
   it ('can decrease the temperature', function(){
     thermostat.decrease()
     expect(thermostat.temp).toEqual(19);
+  });
+
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.decrease();
+    }
+    expect(thermostat.getCurrentTemp()).toEqual(10);
   });
  
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function Thermostat() {
+  this.MINIMUM_TEMP = 10;
   this.temp = 20;
+};
+
+Thermostat.prototype.getCurrentTemp = function() {
+  return this.temp;
 };
 
 Thermostat.prototype.increase = function () {
@@ -9,5 +14,12 @@ Thermostat.prototype.increase = function () {
 };
 
 Thermostat.prototype.decrease = function () {
+  if (this.isMinimumTemp()) {
+    return;
+  }
   this.temp --;
 };
+
+Thermostat.prototype.isMinimumTemp = function() {
+  return this.temp === this.MINIMUM_TEMP;
+}
